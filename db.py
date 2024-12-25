6344f3e0676ea4c9bd414c7dc5060590b42f22b9
Mangoz1x <ryan@fotovat.com>
2024-12-25 02:10:17 -0500

    Created in memory db

--- db.py
+++ db.py
@@ -2,43 +2,17 @@ import json
 import os
 import threading
 
-DATABASE_FILE = 'database.json'
-DATABASE_LOCK = threading.Lock()
-
-def load_database():
-    """Load the database from the JSON file."""
-    if not os.path.exists(DATABASE_FILE):
-        with open(DATABASE_FILE, 'w') as f:
-            json.dump({}, f)
-    with open(DATABASE_FILE, 'r') as f:
-        try:
-            data = json.load(f)
-            return data
-        except json.JSONDecodeError:
-            return {}
-
-def save_database(data):
-    """Save the database to the JSON file."""
-    with open(DATABASE_FILE, 'w') as f:
-        json.dump(data, f, indent=4)
+database = {}
 
 def update_database(key, value):
     """Update a specific key in the database."""
-    with DATABASE_LOCK:
-        db = load_database()
-        db[key] = value
-        save_database(db)
+    database[key] = value
 
 def remove_from_database(keys):
     """Remove specific keys from the database."""
-    with DATABASE_LOCK:
-        db = load_database()
-        for key in keys:
-            db.pop(key, None)
-        save_database(db)
+    for key in keys:
+        database.pop(key, None)
 
 def get_from_database(keys):
     """Retrieve specific keys from the database."""
-    with DATABASE_LOCK:
-        db = load_database()
-        return {key: db.get(key) for key in keys}
+    return {key: database.get(key) for key in keys}
